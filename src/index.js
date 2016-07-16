8e8f4d5f0a9ae13ecfc9299dea835e66694be2b3
David Forero <davefogo@gmail.com>
2016-07-15 20:14:02 -0500

    finish ask for Amazon certification

--- src/index.js
+++ src/index.js
@@ -14,32 +14,44 @@
  *
  * Examples:
  * One-shot model:
- *  User: "Alexa, ask Space Geek for a space fact"
- *  Alexa: "Here's your space fact: ..."
+ *  User: "Alexa, ask Country Guru for a country fact"
+ *  Alexa: "Here's your country fact: ..."
  */
 
 /**
  * App ID for the skill
  */
-var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[arn:aws:lambda:us-east-1:865873523851:function:myFactSkill]";
 
 /**
- * Array containing space facts.
+ * Array containing country facts.
  */
 var FACTS = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
+    "China is the most populous country with 1.37 billion people.",
+    "The second most populous country is India with 1.31 billion people.",
+    "The United States is the third most populous country with 321 million people.",
+    "The least populous country is Cocos (Keeling) Islands with just 596 people.",
+    "The Vatican City has a population of about 1000 people.",
+    "Monaco has a GDP per capita of 163353 US Dollars making it the highest in the world.",
+    "Burundi has a GDP per capita of 275.98 US dollars and is the lowest in the world.",
+    "Qatar has the lowest unemployment rate. It's only 0.3 percent. This means that out of 2.24 million people that live there, only 6720 people are jobless.",
+    "Mauritania has the highest unemployment rate. It's 31 percent. This means that out of 4.07 million people that live there, 1261700 people are jobless.",
+    "In Iceland about 98 out of 100 people are internet users.",
+    "In America Samoa, North Korea and Northern Mariana Islands, 0 out of 100 people are internet users.",
+    "South Korea has the world's fastest internet connection at 26.7 megabits per second.",
+    "Lybia is the country with the slowest internet connection at 0.5 megabits per second.",
+    "Russia is the largest country at 17075200 square kilometers or 6592735 square miles.",
+    "The second largest country is Canada with 9826630 square kilometers or 3855081 square miles.",
+    "The United States is the third largest country with 9826630 square kilometers or 3718691 square miles.",
+    "The Vatican City is the smallest country with 0.44 square kilometers or 0.17 square miles.",
+    "The United States does not have a national official language. However, English is the primary language used in government bodies.",
+    "Canada has two national official languages. English and French.",
+    "The country with the most national official languages is Zimbabwe with 16.",
+    "Finland is the country with the most islands with over 100000.",
+    "There are 44 landlocked countries in the world. This means the country is completely surrounded by land.",
+    "There are 196 countries in the world.",
+    "The country with the most populous city/urban area is Japan. Tokyo/Yokohama has a population of 33200000.",
+    "The United States has the second most populous city/urban area. The New York Metro has a population of 17800000."
 ];
 
 /**
@@ -48,7 +60,7 @@ var FACTS = [
 var AlexaSkill = require('./AlexaSkill');
 
 /**
- * SpaceGeek is a child of AlexaSkill.
+ * CountryGuru is a child of AlexaSkill.
  * To read more about inheritance in JavaScript, see the link below.
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
@@ -85,7 +97,7 @@ Fact.prototype.intentHandlers = {
     },
 
     "AMAZON.HelpIntent": function (intent, session, response) {
-        response.ask("You can say tell me a space fact, or, you can say exit... What can I help you with?", "What can I help you with?");
+        response.ask("You can say tell me a country fact, or, you can say exit... What can I help you with?", "What can I help you with?");
     },
 
     "AMAZON.StopIntent": function (intent, session, response) {
@@ -103,7 +115,7 @@ Fact.prototype.intentHandlers = {
  * Gets a random new fact from the list and returns to the user.
  */
 function handleNewFactRequest(response) {
-    // Get a random space fact from the space facts list
+    // Get a random country fact from the country facts list
     var factIndex = Math.floor(Math.random() * FACTS.length);
     var randomFact = FACTS[factIndex];
 
@@ -115,8 +127,7 @@ function handleNewFactRequest(response) {
 
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
-    // Create an instance of the SpaceGeek skill.
+    // Create an instance of the CountryGuru skill.
     var fact = new Fact();
     fact.execute(event, context);
 };
-
